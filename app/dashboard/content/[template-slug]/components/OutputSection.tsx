5981790d2e74198a33d57e3696427940eb7659a5
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-06-28 14:45:15 +0530

    Text editor developed. ToastUIeditor

--- app/dashboard/content/[template-slug]/components/OutputSection.tsx
+++ app/dashboard/content/[template-slug]/components/OutputSection.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
+
 
 function OutputSection() {
+
+  const editorRef:any=useRef();
+
   return (
-    <div>
-      Output
+    <div className='bg-slate-100 shadow-lg border rounded-lg'>
+    <div className='flex justify-between items-center p-5'>
+       <h2 className='font-medium text-lg'>Result</h2>
+       <Button className='flex gap-2'><Copy className='w-4 h-4'/>Copy Text</Button>
+    </div>
+      <Editor
+       ref={editorRef}
+      
+        initialValue="Your output goes here"
+        initialEditType="wysiwyg"
+        // height="600px"
+        useCommandShortcut={true}
+        onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+  />
     </div>
   )
 }
